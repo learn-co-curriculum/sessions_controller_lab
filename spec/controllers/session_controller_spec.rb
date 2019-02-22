631e4e0424b474ba1cc615a3c5c5855152350d15
maxwellbenton <maxwellbenton@gmail.com>
2019-02-22 08:45:16 -0500

    updates deprecated controller test style

--- spec/controllers/session_controller_spec.rb
+++ spec/controllers/session_controller_spec.rb
@@ -9,33 +9,33 @@ RSpec.describe SessionsController, type: :controller do
     end
 
     it 'redirects to login page if :name is empty' do
-      post :create, name: ''
+      post :create, params: { name: '' }
       expect(response).to redirect_to controller: 'sessions', action: 'new'
     end
 
     it 'sets session[:name] if :name was given' do
       me = 'Werner Brandes'
-      post :create, name: me
+      post :create, params: { name: me }
       expect(@request.session[:name]).to eq me
     end
 
     it 'redirects to "/" if logged in' do
       me = 'Werner Brandes'
-      post :create, name: me
+      post :create, params: { name: me }
       expect(response).to redirect_to('/')
     end
 
 
   end
 
-  describe 'post destroy' do    
+  describe 'post destroy' do
     it 'leaves session[:name] nil if it was not set' do
       post :destroy
       expect(@request.session[:name]).to be nil
     end
 
     it 'clears session[:name] if it was set' do
-      post :create, name: 'Trinity'
+      post :create, params: { name: 'Trinity' }
       expect(@request.session[:name]).not_to be nil
       post :destroy
       expect(@request.session[:name]).to be nil
